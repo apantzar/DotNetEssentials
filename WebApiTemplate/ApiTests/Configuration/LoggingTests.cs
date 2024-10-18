5bc87fd0f6f5fc505abef38398e02b4c448b6c7b
Kitsatis Vasilios <vasilios.kitsatis@infoteam-software.gr>
2024-10-18 16:57:30 +0300

    Adds ApiTests project
    Creates and configures ApiTests to set up a new test project for the API, including necessary package references and project settings. Adds `AppSettingsTests`, `LoggingTests`, and `StartupTests` classes to test configuration binding, Serilog logging, and application startup respectively.

---
+++ WebApiTemplate/ApiTests/Configuration/LoggingTests.cs
@@ -0,0 +1,43 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using Serilog;
+
+namespace ApiTests.Configuration
+{
+    public class LoggingTests
+    {
+        [Fact]
+        public void Serilog_IsConfigured_Correctly()
+        {
+            // Arrange
+            var logEvents = new List<LogEvent>();
+            var sink = new DelegatingSink(logEvents);
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Sink(sink)
+                .CreateLogger();
+
+            // Act
+            logger.Information("Test log message");
+
+            // Assert
+            Assert.Single(logEvents);
+            Assert.Equal("Test log message", logEvents[0].MessageTemplate.Text);
+        }
+
+        private class DelegatingSink : ILogEventSink
+        {
+            private readonly List<LogEvent> _logEvents;
+
+            public DelegatingSink(List<LogEvent> logEvents)
+            {
+                _logEvents = logEvents;
+            }
+
+            public void Emit(LogEvent logEvent)
+            {
+                _logEvents.Add(logEvent);
+            }
+        }
+    }
+}
---
+++ WebApiTemplate/ApiTests/StartupTests.cs
@@ -0,0 +1,31 @@
+﻿using Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTests
+{
+    public class StartupTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public StartupTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Application_Starts_Without_Errors()
+        {
+            // Act
+            var client = _factory.CreateClient();
+
+            // Assert
+            var response = await client.GetAsync("/swagger");
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
